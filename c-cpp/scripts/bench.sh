a7e0b3085875b6299e1df2226216454209ed0aff
Jerry Zhang <jerryzh168@gmail.com>
2016-12-02 15:48:55 -0500

    bench scripts

--- c-cpp/scripts/bench.sh
+++ c-cpp/scripts/bench.sh
@@ -2,7 +2,8 @@
 
 threads=(1 2 4 8 16 20 32)
 
+touch result
 for i in "${threads[@]}"
 do
-    ../bin/lockfree-hashtable -L 1 -n 0 -d 10000 -t $i
+    bin/lockfree-hashtable -L 1 -n 0 -d 10000 -t $i >> result
 done
