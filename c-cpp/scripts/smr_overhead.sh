e086dcf573d22b598fc15ada3c7b0b5cf41da75e
Jerry Zhang <jerryzh168@gmail.com>
2016-12-11 23:06:43 -0500

    Removed some old scripts

--- c-cpp/scripts/smr_overhead.sh
+++ c-cpp/scripts/smr_overhead.sh
@@ -6,6 +6,8 @@ report_name=results/$test_name.report
 rm -f $report_name
 touch $report_name
 
+mkdir -p results/$test_name
+
 lf=50
 update=0
 for thread in "${threads[@]}"
@@ -15,7 +17,7 @@ do
     bucket_num=$(($thread * 10))
     size=$(($bucket_num * $lf))
     echo "Thread: $thread Bucket Num: $bucket_num LF: $lf Update: $update"
-    file_name=results/$test_name'_'$thread'_'$bucket_num'_'$i
+    file_name=results/$test_name/$test_name'_'$thread'_'$bucket_num'_'$i
     bin/lockfree-hashtable -i $size -d $duration -t $thread -S $seed -u $update -l $lf -p $interval -L 1 -n 0 > $file_name
     #python scripts/get_stats.py -f $file_name
     #python scripts/get_stats.py -f $file_name >> $report_name
