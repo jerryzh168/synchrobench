8c490bdeb77f80dcccc5588e547a9aafe5ce6515
Ziqi Wang <675973885@qq.com>
2016-12-13 19:13:31 +0000

    fix memory leak in harris insert

--- c-cpp/src/linkedlists/lockfree-list/harris.c
+++ c-cpp/src/linkedlists/lockfree-list/harris.c
@@ -138,7 +138,10 @@ int harris_insert(intset_t *set, val_t val) {
 			}
 			return 0;
 		}
-		newnode = new_node(val, right_node, 0);
+		if(newnode == NULL)
+			newnode = new_node(val, right_node, 0);
+		else
+			newnode->next = right_node;
 		if (ATOMIC_CAS_MB(&left_node->next, right_node, newnode))
 			return 1;
 	} while(1);
