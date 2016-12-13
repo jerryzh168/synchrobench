1e51e31510761cedfa172dc2d3846f258ff41e5f
Jerry Zhang <jerryzh168@gmail.com>
2016-12-13 17:22:31 -0500

    update run all

--- c-cpp/scripts/run_all.sh
+++ c-cpp/scripts/run_all.sh
@@ -1,7 +1,14 @@
 #/bin/bash
+TEST=leaky
 make clean
 make lockfree
 ./scripts/smr_overhead.sh
 ./scripts/rw_dominant.sh
 ./scripts/op_len.sh
 ./scripts/contention.sh
+python scripts/get_stats.py -d results/contention/
+python scripts/get_stats.py -d results/smr_overhead/
+python scripts/get_stats.py -d results/rw_dominant/
+python scripts/get_stats.py -d results/op_len/
+rm -r ../../results/$TEST
+cp results ../../results/%TEST
