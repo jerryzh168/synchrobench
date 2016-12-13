5c934c11477d9619d9bfcaf7c39e3b0418f80a35
Ziqi Wang <675973885@qq.com>
2016-12-13 00:32:13 +0000

    Test scrtips

--- c-cpp/scripts/op_len.sh
+++ c-cpp/scripts/op_len.sh
@@ -1,22 +1,26 @@
 #/bin/sh
 source scripts/bench.sh
 
-test_name=scale
+test_name=op_len
 report_name=results/$test_name.report
 rm -f $report_name
 touch $report_name
 
-for thread in "${threads[@]}"
+mkdir -p results/$test_name
+
+bucket_num=20
+thread=20
+update=20
+for lf in "${lfs[@]}"
 do
-    for lf in "${lfs[@]}"
-    do
-	for update in "${updates[@]}"
-	do
-	    echo "Thread: $thread LF: $lf Update: $update"
-	    name=results/scale'_'$thread'_'$lf'_'$update
-	    bin/lockfree-hashtable -i $size -d $duration -t $thread -S $seed -u $update -l $lf -p $interval > $name
-	    python scripts/get_stats.py -f $name
-	    python scripts/get_stats.py -f $name >> $report_name
-	done
-    done
+  for i in "${itr[@]}"
+  do
+    size=$(($bucket_num * $lf))
+    echo "Thread: $thread Bucket Num: $bucket_num LF: $lf Update: $update"
+    file_name=results/$test_name/$test_name'_'$lf'_'$i
+    bin/lockfree-hashtable -i $size -d $duration -t $thread -S $seed -u $update -l $lf -p $interval -L 1 -n 0 > $file_name
+ #   python scripts/get_stats.py -f $file_name
+ #   python scripts/get_stats.py -f $file_name >> $report_name
+  done
 done
+
