71959215eaafe4145643f4c23056fc21651e75b1
Ziqi Wang <675973885@qq.com>
2016-12-14 00:48:55 +0000

    run all script

--- c-cpp/scripts/run_all.sh
+++ c-cpp/scripts/run_all.sh
@@ -1,5 +1,14 @@
 #/bin/bash
+TEST=epoch
+make clean
+make lockfree
 ./scripts/smr_overhead.sh
 ./scripts/rw_dominant.sh
 ./scripts/op_len.sh
 ./scripts/contention.sh
+python scripts/get_stats.py -d results/contention/
+python scripts/get_stats.py -d results/smr_overhead/
+python scripts/get_stats.py -d results/rw_dominant/
+python scripts/get_stats.py -d results/op_len/
+rm -r ../../results/$TEST
+cp -r results ../../results/$TEST
